6da31fe990be59787741e59d5b0382bd593bbcb0
sunxu <ntsunxu@163.com>
2019-12-03 21:57:51 +0800

    添加 263, 264 题

---
+++ src/main/java/q300/Q263_UglyNumber.java
@@ -0,0 +1,68 @@
+package q300;
+
+import org.junit.runner.RunWith;
+import util.runner.Answer;
+import util.runner.DataExpectation;
+import util.runner.LeetCodeRunner;
+import util.runner.TestData;
+
+/**
+ * https://leetcode.com/problems/ugly-number/
+ *
+ * Write a program to check whether a given number is an ugly number.
+ *
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ *
+ * Example 1:
+ *
+ * Input: 6
+ * Output: true
+ * Explanation: 6 = 2 × 3
+ *
+ * Example 2:
+ *
+ * Input: 8
+ * Output: true
+ * Explanation: 8 = 2 × 2 × 2
+ *
+ * Example 3:
+ *
+ * Input: 14
+ * Output: false
+ * Explanation: 14 is not ugly since it includes another prime factor 7.
+ *
+ * Note:
+ *
+ * 1 is typically treated as an ugly number.
+ * Input is within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+ */
+@RunWith(LeetCodeRunner.class)
+public class Q263_UglyNumber {
+
+    @Answer
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+
+    @TestData
+    public DataExpectation example1 = DataExpectation.create(6).expect(true);
+
+    @TestData
+    public DataExpectation example2 = DataExpectation.create(8).expect(true);
+
+    @TestData
+    public DataExpectation example3 = DataExpectation.create(14).expect(false);
+
+}
---
+++ src/main/java/q300/Q264_UglyNumberII.java
@@ -0,0 +1,101 @@
+package q300;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.runner.RunWith;
+import util.runner.Answer;
+import util.runner.DataExpectation;
+import util.runner.LeetCodeRunner;
+import util.runner.TestData;
+
+/**
+ * https://leetcode.com/problems/ugly-number-ii/
+ *
+ * Write a program to find the n-th ugly number.
+ *
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ *
+ * Example:
+ *
+ * Input: n = 10
+ * Output: 12
+ * Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+ *
+ * Note:
+ *
+ * 1 is typically treated as an ugly number.
+ * n does not exceed 1690.
+ */
+@RunWith(LeetCodeRunner.class)
+public class Q264_UglyNumberII {
+
+    // n 序号从1 开始.
+    // 晚上的解法
+    @Answer
+    public int nthUglyNumber(int n) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        int i2 = 0, i3 = 0, i5 = 0;
+        while (nums.size() < n) {
+            int m2 = nums.get(i2) * 2, m3 = nums.get(i3) * 3, m5 = nums.get(i5) * 5;
+            int min = Math.min(m2, Math.min(m3, m5));
+            nums.add(min);
+            if (min == m2) {
+                ++i2;
+            }
+            if (min == m3) {
+                ++i3;
+            }
+            if (min == m5) {
+                ++i5;
+            }
+        }
+        return nums.get(nums.size() - 1);
+    }
+
+    // LeetCode 上最快的解法
+    @Answer
+    public int leetCode(int n) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        int factor2 = 2;
+        int factor3 = 3;
+        int factor5 = 5;
+        for (int i = 1; i < n; i++) {
+            int min = Math.min(Math.min(factor2, factor3), factor5);
+            ugly[i] = min;
+            if (factor2 == min) {
+                factor2 = 2 * ugly[++index2];
+            }
+            if (factor3 == min) {
+                factor3 = 3 * ugly[++index3];
+            }
+            if (factor5 == min) {
+                factor5 = 5 * ugly[++index5];
+            }
+        }
+        return ugly[n - 1];
+    }
+
+    @TestData
+    public DataExpectation example = DataExpectation.create(10).expect(12);
+
+    @TestData
+    public DataExpectation normal1 = DataExpectation.create(1).expect(1);
+
+    @TestData
+    public DataExpectation normal2 = DataExpectation.create(1).expect(1);
+
+    @TestData
+    public DataExpectation normal3 = DataExpectation.create(2).expect(2);
+
+    @TestData
+    public DataExpectation normal4 = DataExpectation.create(3).expect(3);
+
+    @TestData
+    public DataExpectation normal7 = DataExpectation.create(7).expect(8);
+
+}
