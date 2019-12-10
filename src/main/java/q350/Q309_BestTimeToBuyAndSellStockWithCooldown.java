f9f6554b80026f72b964ac9c5a44e06c58d0ab47
sunxu <ntsunxu@163.com>
2019-12-10 18:52:10 +0800

    添加 309 题的题目解释

--- src/main/java/q350/Q309_BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/q350/Q309_BestTimeToBuyAndSellStockWithCooldown.java
@@ -32,19 +32,26 @@ public class Q309_BestTimeToBuyAndSellStockWithCooldown {
     // https://soulmachine.gitbooks.io/algorithm-essentials/java/dp/best-time-to-buy-and-sell-stock-with-cooldown.html
     @Answer
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
+        final int n = prices.length;
+        if (n < 2) {
             return 0;
         }
-        int[] sell = new int[prices.length];
-        int[] buy = new int[prices.length];
+        // 表示当天卖出获得的最大利润
+        int[] sell = new int[n];
+        // 表示当天买入获得的最大利润
+        int[] buy = new int[n];
         sell[0] = 0;
         buy[0] = -prices[0];
 
-        for (int i = 1; i < prices.length; ++i) {
+        for (int i = 1; i < n; ++i) {
+            // 今天卖出, 最大利润有两种可能: 一是今天没动作跟昨天未持股状态一样, 二是今天卖了股票
             sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+            // 今天买入, 最大利润有两种可能: 一是今天没动作跟昨天持股状态一样,
+            // 二是前天卖了股票, 今天买了股票, 因为 cooldown 只能隔天交易, 所以今天买股票要追溯到前天的状态
             buy[i] = Math.max(buy[i - 1], (i > 1 ? sell[i - 2] : 0) - prices[i]);
         }
-        return sell[prices.length - 1];
+        // 最后一天卖出即最大的利润
+        return sell[n - 1];
     }
 
     @TestData
