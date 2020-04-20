bd75f7444d1407d65e5b14ad83f32149406ad5be
sunxuia <ntsunxu@163.com>
2020-04-20 20:57:59 +0800

    add: 470, 472

---
+++ src/main/java/q500/Q470_ImplementRand10UsingRand7.java
@@ -0,0 +1,81 @@
+package q500;
+
+import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * https://leetcode.com/problems/implement-rand10-using-rand7/
+ *
+ * Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which
+ * generates a uniform random integer in the range 1 to 10.
+ *
+ * Do NOT use system's Math.random().
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: 1
+ * Output: [7]
+ *
+ * Example 2:
+ *
+ * Input: 2
+ * Output: [8,4]
+ *
+ * Example 3:
+ *
+ * Input: 3
+ * Output: [8,1,10]
+ *
+ *
+ *
+ * Note:
+ *
+ * rand7 is predefined.
+ * Each testcase has one argument: n, the number of times that rand10 is called.
+ *
+ *
+ *
+ * Follow up:
+ *
+ * What is the expected value for the number of calls to rand7() function?
+ * Could you minimize the number of calls to rand7()?
+ */
+public class Q470_ImplementRand10UsingRand7 {
+
+    private static abstract class SolBase {
+
+        protected int rand7() {
+            return new Random().nextInt(7) + 1;
+        }
+    }
+
+    private static class Solution extends SolBase {
+
+        // leetcode 中最快的方法将i 变成类变量, 这样每次调用都只需要i++ 就行了.
+        public int rand10() {
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum += i * 7 + rand7() - 1;
+            }
+            return sum % 10 + 1;
+        }
+    }
+
+    @Test
+    public void testMethod() {
+        Solution solution = new Solution();
+        final int total = 10000;
+        int[] count = new int[10];
+        for (int i = 0; i < total; i++) {
+            count[solution.rand10() - 1]++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            double ratio = ((double) count[i]) / total;
+            Assert.assertTrue(String.format("ratio of %d is %f", i + 1, ratio), 0.09 < ratio && ratio < 0.11);
+        }
+    }
+}
---
+++ src/main/java/q500/Q472_ConcatenatedWords.java
@@ -0,0 +1,116 @@
+package q500;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.junit.runner.RunWith;
+import util.runner.Answer;
+import util.runner.LeetCodeRunner;
+import util.runner.TestData;
+import util.runner.data.DataExpectation;
+import util.runner.data.TestDataFileHelper;
+
+/**
+ * https://leetcode.com/problems/concatenated-words/
+ *
+ * Given a list of words (without duplicates), please write a program that returns all concatenated words in the
+ * given list of words.
+ *
+ * A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given
+ * array.
+ *
+ * Example:
+ *
+ * Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+ *
+ * Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+ *
+ * Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats";
+ * "dogcatsdog" can be concatenated by "dog", "cats" and "dog";
+ * "ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+ *
+ * Note:
+ *
+ * The number of elements of the given array will not exceed 10,000
+ * The length sum of elements in the given array will not exceed 600,000.
+ * All the input string will only include lower case letters.
+ * The returned elements order does not matter.
+ */
+@RunWith(LeetCodeRunner.class)
+public class Q472_ConcatenatedWords {
+
+    /**
+     * 参考 {@link q150.Q139_WordBreak} 中的解答, 思路类似.
+     * 为了避免超时, 将遍历字典改为使用字典树的方式.
+     */
+    @Answer
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        List<String> res = new ArrayList<>();
+        final Dict root = new Dict();
+        for (String word : words) {
+            final int len = word.length();
+            if (len == 0) {
+                continue;
+            }
+            boolean[] dp = new boolean[len + 1];
+            dp[0] = true;
+            for (int i = 0; i < len; i++) {
+                if (dp[i]) {
+                    // 当前位置和之前都有匹配, 则遍历字典树, 找出下一个匹配的单词
+                    Dict dict = root.children[word.charAt(i) - 'a'];
+                    for (int j = i + 1; dict != null; j++) {
+                        dp[j] = dp[j] || dict.isWord;
+                        dict = j == len ? null : dict.children[word.charAt(j) - 'a'];
+                    }
+                }
+            }
+            if (dp[len]) {
+                // 组合词, 放入结果
+                res.add(word);
+            } else {
+                // 单词, 加入字典树
+                Dict dict = root;
+                for (int i = 0; i < len; i++) {
+                    int idx = word.charAt(i) - 'a';
+                    if (dict.children[idx] == null) {
+                        dict.children[idx] = new Dict();
+                    }
+                    dict = dict.children[idx];
+                }
+                dict.isWord = true;
+            }
+        }
+        return res;
+    }
+
+    private static class Dict {
+
+        boolean isWord;
+
+        Dict[] children = new Dict[26];
+
+    }
+
+    @TestData
+    public DataExpectation example = DataExpectation.builder()
+            .addArgument(new String[]{"cat", "cats", "catsdogcats", "dog",
+                    "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"})
+            .expect(new String[]{"catsdogcats", "dogcatsdog", "ratcatdogcat"})
+            .unorderResult()
+            .build();
+
+    @TestData
+    public DataExpectation border0 = DataExpectation.create(new String[0]).expect(Collections.emptyList());
+
+    @TestData
+    public DataExpectation border1 = DataExpectation.create(new String[]{""}).expect(Collections.emptyList());
+
+    @TestData
+    public DataExpectation overTime = DataExpectation
+            .create(TestDataFileHelper.readStringArray("Q472_LongTestData"))
+            .expect(TestDataFileHelper.readStringArray("Q472_LongTestData_Result"));
+
+}
