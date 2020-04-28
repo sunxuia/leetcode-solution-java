4d758a6a69a9e3d718e64f04f299ffd6b507d2d5
sunxuia <ntsunxu@163.com>
2020-04-28 19:26:04 +0800

    fix: 未提交更改

--- src/main/java/util/runner/data/LazyDataProvider.java
+++ src/main/java/util/runner/data/LazyDataProvider.java
@@ -1,5 +1,7 @@
 package util.runner.data;
 
+import java.util.function.Function;
+
 @FunctionalInterface
 public interface LazyDataProvider<T> {
 
@@ -7,6 +9,13 @@ public interface LazyDataProvider<T> {
 
     static <T> LazyDataProvider<T> create(LazyDataProvider<T> provider) {
         return provider;
+    }
+
+    default <R> LazyDataProvider<R> then(Function<T, R> converter) {
+        return () -> {
+            T val = get();
+            return converter.apply(val);
+        };
     }
 
 }
